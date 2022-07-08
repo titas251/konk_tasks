d9ae0db43982fecb8417a9dd7c10c17db755ad36
titas251 <krtitas1@gmail.com>
2022-07-08 17:38:40 +0300

    Added country enum

--- src/app.ts
+++ src/app.ts
@@ -6,41 +6,51 @@ enum Genre {
   'fantasy',
 }
 
+enum Country {
+  'US',
+  'UK',
+  'France',
+}
+
 interface IMovie {
   director: string;
-  genre: Genre;
+  genres: Genre[];
   title: string;
+  country: Country;
   duration: number;
   rating: number;
 }
 
 const movieTitanic = {
   director: 'Mark Z',
-  genre: Genre.drama,
+  genres: [Genre.drama, Genre.fantasy],
   title: 'Titanic',
+  country: Country.US,
   duration: 126,
   rating: 7.63,
 };
 
 const movieDjango = {
   director: 'Mark T',
-  genre: Genre.action,
+  genres: [Genre.action],
   title: 'Django',
+  country: Country.US,
   duration: 102,
 };
 
 const isMovie = (obj: any): obj is IMovie => {
   return (
     typeof obj.director === 'string' &&
-    typeof obj.genre === 'number' &&
+    obj.genres.every((value: any) => typeof value === 'number') === true &&
     typeof obj.title === 'string' &&
+    typeof obj.country === 'number' &&
     typeof obj.duration === 'number' &&
     typeof obj.rating === 'number'
   );
 };
 
-//console.log(isMovie(movieTitanic));  //true
-//console.log(isMovie(movieDjango));  //false
+console.log(isMovie(movieTitanic)); //true
+console.log(isMovie(movieDjango)); //false
 
 // #6
 const getRandomInt = (min: number, max: number): number => {
@@ -49,11 +59,15 @@ const getRandomInt = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+const genres: Genre[] = [Genre.action, Genre.drama, Genre.fantasy];
+const countries: Country[] = [Country.US, Country.France, Country.UK];
+
 const getRandomMovieObject = (): IMovie => {
   return {
     director: faker.name.findName(),
-    genre: getRandomInt(0, Object.keys(Genre).length / 2),
+    genres: faker.helpers.arrayElements(genres),
     title: `${faker.word.adjective()} ${faker.word.noun()}`,
+    country: faker.helpers.arrayElement(countries),
     duration: getRandomInt(30, 200),
     rating: faker.datatype.float({ min: 1, max: 10 }),
   };
